99fa006daac40d01f27445a9f616fb3d3315809d
Lingyi Wang <lywang07@gmail.com>
2017-03-08 15:56:39 -0800

    change the input tar.gz file to zip file

--- build_pkg.sh
+++ build_pkg.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-SOURCE=`find /in -name 'kolibri*.tar.gz' -type f -exec basename {} ';'`
-echo -e "---source file is $SOURCE---\n"
 
-echo -e "---Getting rpm package from tar.gz file by using fpm---\n"
-RPMPACKAGE=`fpm -s tar -t rpm /in/$SOURCE`
+set -eo pipefail
+
+ZIPFILE=`find /in -name 'kolibri*.zip' -type f -exec basename {} ';'`
+echo -e "---unzip file $ZIPFILE---\n"
+unzip /in/$ZIPFILE
+SOURCE=${ZIPFILE%.*}
+echo -e "---Getting rpm package by using fpm---\n"
+RPMPACKAGE=`fpm -s python -t rpm --no-python-fix-name $SOURCE/setup.py`
 NAME=`grep -o 'kolibri.*rpm' <<< "$RPMPACKAGE"`
 
 echo -e "---Successfully get $NAME---"
